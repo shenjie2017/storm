1d8274ebb51b0f7b7909c050dc8764482cbe6471
shenjie <1075850619@qq.com>
2018-05-29 15:37:56 +0800

    kafka+storm的消费者整合demo

---
+++ flume-kafka-storm/src/main/java/com/blue/bolt/MyBolt1.java
@@ -0,0 +1,55 @@
+package com.blue.bolt;
+
+import org.apache.storm.task.OutputCollector;
+import org.apache.storm.task.TopologyContext;
+import org.apache.storm.topology.IBasicBolt;
+import org.apache.storm.topology.IRichBolt;
+import org.apache.storm.topology.IWindowedBolt;
+import org.apache.storm.topology.OutputFieldsDeclarer;
+import org.apache.storm.topology.base.BaseRichBolt;
+import org.apache.storm.tuple.Tuple;
+
+import java.util.Map;
+
+/**
+ * @author shenjie
+ * @version v1.0
+ * @Description
+ * @Date: Create in 11:20 2018/5/29
+ * @Modifide By:
+ **/
+
+//      ┏┛ ┻━━━━━┛ ┻┓
+//      ┃　　　　　　 ┃
+//      ┃　　　━　　　┃
+//      ┃　┳┛　  ┗┳　┃
+//      ┃　　　　　　 ┃
+//      ┃　　　┻　　　┃
+//      ┃　　　　　　 ┃
+//      ┗━┓　　　┏━━━┛
+//        ┃　　　┃   神兽保佑
+//        ┃　　　┃   代码无BUG！
+//        ┃　　　┗━━━━━━━━━┓
+//        ┃　　　　　　　    ┣┓
+//        ┃　　　　         ┏┛
+//        ┗━┓ ┓ ┏━━━┳ ┓ ┏━┛
+//          ┃ ┫ ┫   ┃ ┫ ┫
+//          ┗━┻━┛   ┗━┻━┛
+
+public class MyBolt1 extends BaseRichBolt {
+    public void prepare(Map stormConf, TopologyContext context, OutputCollector collector) {
+
+    }
+
+    public void execute(Tuple input) {
+        byte[] value = (byte[]) input.getValue(0);
+        String msg = new String(value);
+        System.out.println("thread-id:"+Thread.currentThread().getId()+" msg:"+msg);
+    }
+
+    public void declareOutputFields(OutputFieldsDeclarer declarer) {
+
+    }
+
+
+}
---
+++ flume-kafka-storm/src/main/java/com/blue/boot/KafkaAndStormTopologyMain.java
@@ -0,0 +1,58 @@
+package com.blue.boot;
+
+import com.blue.bolt.MyBolt1;
+import org.apache.storm.Config;
+import org.apache.storm.LocalCluster;
+import org.apache.storm.StormSubmitter;
+import org.apache.storm.generated.AlreadyAliveException;
+import org.apache.storm.generated.AuthorizationException;
+import org.apache.storm.generated.InvalidTopologyException;
+import org.apache.storm.kafka.KafkaSpout;
+import org.apache.storm.kafka.SpoutConfig;
+import org.apache.storm.kafka.ZkHosts;
+import org.apache.storm.topology.TopologyBuilder;
+
+/**
+ * @author shenjie
+ * @version v1.0
+ * @Description
+ * @Date: Create in 10:17 2018/5/29
+ * @Modifide By:
+ **/
+
+//      ┏┛ ┻━━━━━┛ ┻┓
+//      ┃　　　　　　 ┃
+//      ┃　　　━　　　┃
+//      ┃　┳┛　  ┗┳　┃
+//      ┃　　　　　　 ┃
+//      ┃　　　┻　　　┃
+//      ┃　　　　　　 ┃
+//      ┗━┓　　　┏━━━┛
+//        ┃　　　┃   神兽保佑
+//        ┃　　　┃   代码无BUG！
+//        ┃　　　┗━━━━━━━━━┓
+//        ┃　　　　　　　    ┣┓
+//        ┃　　　　         ┏┛
+//        ┗━┓ ┓ ┏━━━┳ ┓ ┏━┛
+//          ┃ ┫ ┫   ┃ ┫ ┫
+//          ┗━┻━┛   ┗━┻━┛
+
+public class KafkaAndStormTopologyMain {
+
+    public static void main(String[] args) throws InvalidTopologyException, AuthorizationException, AlreadyAliveException {
+        TopologyBuilder builder = new TopologyBuilder();
+        SpoutConfig config = new SpoutConfig(new ZkHosts("192.168.163.111:2181"),"orderMq","/mykafka","kafkaSpout");
+        builder.setSpout("kafkaSpout",new KafkaSpout(config),1);
+        builder.setBolt("mybolt1",new MyBolt1(),1).shuffleGrouping("kafkaSpout");
+
+        Config conf = new Config();
+        //打印调试信息
+        // conf.setDebug(true);
+        if (args!=null && args.length>0) {
+            StormSubmitter.submitTopology(args[0], conf, builder.createTopology());
+        }else {
+            LocalCluster localCluster = new LocalCluster();
+            localCluster.submitTopology("storm2kafka", conf, builder.createTopology());
+        }
+    }
+}
